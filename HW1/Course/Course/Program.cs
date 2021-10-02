11b2cf59693ce3cff9e6d6ab91d96a21022c60b3
tony_su <susu2721404@gmail.com>
2021-10-03 01:54:32 +0800

    加入專案檔案。

---
+++ HW1/Course/Course/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form selectCourseForm = new SelectCourseForm();
+            Application.Run(selectCourseForm);
+        }
+    }
+}
---
+++ HW1/Course/Course/SelectCourseForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course
+{
+    public partial class SelectCourseForm : Form
+    {
+        public SelectCourseForm()
+        {
+            InitializeComponent();
+            DataGridView recordDataGridView = new DataGridView();
+            recordDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            recordDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+
+            DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
+            DataGridViewCheckBoxColumn col2 = new DataGridViewCheckBoxColumn();
+
+            col1.HeaderText = "Column1";
+            col2.HeaderText = "Column2";
+
+            recordDataGridView.Columns.AddRange(new DataGridViewColumn[] { col1, col2 });
+
+
+            //recordDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[]{col1, col2});
+            recordDataGridView.Name = "recordDGV";
+            recordDataGridView.ReadOnly = true;
+            recordDataGridView.RowHeadersVisible = true;
+            recordDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            recordDataGridView.Parent = this;
+        }
+
+    }
+}
